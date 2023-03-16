961e371a9f27087658288b4b517af9383c3537bb
Quivcy92 <sotyphilips@yahoo.com>
2023-03-16 16:28:21 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints "Programming is like building a multilingual
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
